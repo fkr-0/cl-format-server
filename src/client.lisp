30972cb8d2a26bbbf9bfe75d0c1c93e688de178a
cbadger <cbadger@not.existent>
2023-09-30 19:21:49 +0200

    refactor core + extend client code

--- src/client.lisp
+++ src/client.lisp
@@ -1,12 +1,11 @@
 (in-package :cl-format-server)
 
-
-(defun send-request (source-code  &key formatter linter port)
+(defun send-request (source-code  &key formatter port)
   "Send a request to the server and return the response."
   (let ((p (or port *default-server-port*)))
     (log-debug "Sending request to port ~A" p)
     (usocket:with-client-socket (socket stream "localhost" p)
-      (let ((request (list :source-code source-code :formatter formatter :linter linter)))
+      (let ((request (list formatter source-code)))
         ;; (log-debug "Sending request: ~A" request)
         (write request :stream stream)
         ;; (log-debug "Finished writing request")
@@ -15,20 +14,6 @@
         (usocket:socket-shutdown socket :output)
         (read stream :eof t)
         ))))
-;; (usocket:with-client-socket (socket stream "localhost" port)
-;;   (let ((request (list :source-code source-code :formatter formatter :linter linter)))
-;;     (log-debug "Sending request: ~A" request)
-;;     (write request :stream stream)
-;;     (finish-output stream)
-;;     (read stream)))
 
-;; (defun format (source-code &key formatter linter port)
-;;   "Format the source-code using the formatter and linter."
-;;   (let ((response (send-request source-code :formatter formatter :linter linter :port port)))
-;;     (log-debug "Received response: ~A" response)
-;;     (getf response :formatted-code)))
-
-
-
-;; (send-request "(defvar a 2) (defvar b 3)" :formatter :trivial-formatter :linter :cl-lint :port 8080)
-;; (send-request "(defvar a 2)" :port 8080)
+(defun simple-req (str port)
+  (send-request str :formatter :trivial-formatter :port port))
